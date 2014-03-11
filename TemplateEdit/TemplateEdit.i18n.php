7438e9d9764da7d15e5e73c3052ae4bbf2d84171
GeneralVolkoV <generalvolkov@gmx.de>
2014-03-11 20:03:20 +0100

    Changes in i18n as in Wiki page

--- TemplateEdit/TemplateEdit.i18n.php
+++ TemplateEdit/TemplateEdit.i18n.php
@@ -1,9 +1,8 @@
 <?php
-
+ 
 $messages = array();
-
+ 
 $messages['en'] = array(
-	// user messages
 	'templateedit'                        => 'Template Edit',
 	'templateedit-description'            => 'Edit Form for used Templates',
 	'templateedit-unknown'                => 'Unknown Article',
@@ -13,39 +12,44 @@ $messages['en'] = array(
 	'templateedit-showselecttemplateform' => 'Please select the template you would like to edit:',
 	'templateedit-showeditform'           => 'Please edit the template:',
 	'templateedit-templatecreated'        => 'Template $1 created',
-	'templateedit-createwithtemplate'     => 'Create with [[Template:$1]]:',
+	'templateedit-createwithtemplate'     => 'Create with [[{{ns:Template}}:$1]]:',
 	'templateedit-templateeditortitle'    => 'Template:$1/Editor',
 	'templateedit-noeditor'               => 'Error, no editor defined!',
 	'templateedit-mustfield'              => 'Obligatory!',
 	'templateedit-remove'                 => 'Remove',
-	'templateedit-editformintro'          => '[[:$1]] - definition of editor: [[Template:$2/Editor]]',
+	'templateedit-editformintro'          => '[[$1]] - definition of editor: [[{{ns:Template}}:$2/Editor]]',
 	'templateedit-fieldundefined'         => 'Field is not (yet) defined!',
 	'templateedit-oldfieldstitle'         => 'Old Fields',
 	'templateedit-oldfield'               => 'Field is not valid (anymore) or not defined!',
 	'templateedit-articlesaved'           => 'Article saved.',
 	'templateedit-templateedited'         => 'Template $1 edited.',
 );
-
+ 
 $messages['de'] = array(
-	// user messages
-	'templateedit'                        => 'Vorlagen Editor',
-	'templateedit-description'            => 'Formular zum Bearbeiten benutzter Templates',
+	'templateedit'                        => 'Vorlageneditor',
+	'templateedit-description'            => 'Stellt ein Formular zum Bearbeiten genutzter Vorlagen bereit',
 	'templateedit-unknown'                => 'Unbekannter Artikel',
 	'templateedit-continue'               => 'Weiter',
 	'templateedit-save'                   => 'Speichern',
-	'templateedit-showstartingform'       => 'Bitte geben Sie den Artikel ein, den Sie editieren wollen:',
-	'templateedit-showselecttemplateform' => 'Bitte w&auml;hlen Sie die Vorlage, die Sie editieren wollen:',
-	'templateedit-showeditform'           => 'Bitte editieren Sie die Vorlage:',
-	'templateedit-templatecreated'        => 'Vorlage $1 erstellt',
-	'templateedit-createwithtemplate'     => 'Erstellen mit [[Vorlage:$1]]:',
+	'templateedit-showstartingform'       => 'Bitte gib den den Artikel ein, den du bearbeiten möchtest:',
+	'templateedit-showselecttemplateform' => 'Bitte w&auml;hl die Vorlage, die du bearbeiten möchtest:',
+	'templateedit-showeditform'           => 'Bitte beabeite die Vorlage:',
+	'templateedit-templatecreated'        => 'Vorlage „$1“ erstellt',
+	'templateedit-createwithtemplate'     => 'Erstellen mit [[{{ns:Template}}:$1]]:',
 	'templateedit-templateeditortitle'    => 'Vorlage:$1/Editor',
-	'templateedit-noeditor'               => 'Fehler, Editor nicht definiert!',
+	'templateedit-noeditor'               => 'Fehler: Editor nicht definiert.',
 	'templateedit-mustfield'              => 'Pflichtfeld!',
 	'templateedit-remove'                 => 'Entfernen',
-	'templateedit-editformintro'          => '[[:$1]] - Editor-Definition unter: [[Vorlage:$2/Editor]]',
+	'templateedit-editformintro'          => '[[$1]] - Editordefinition unter: [[{{ns:Template}}:$2/Editor]]',
 	'templateedit-fieldundefined'         => 'Dieses Feld ist (noch) nicht gef&uuml;llt!',
 	'templateedit-oldfieldstitle'         => 'Veraltete Felder',
-	'templateedit-oldfield'               => 'Dieses Feld ist nicht (mehr) vorhanden oder nicht definiert!',
+	'templateedit-oldfield'               => 'Dieses Feld ist nicht (mehr) vorhanden oder nicht definiert.',
 	'templateedit-articlesaved'           => 'Artikel gespeichert.',
-	'templateedit-templateedited'         => 'Vorlage $1 editiert.',
+	'templateedit-templateedited'         => 'Vorlage „$1“ bearbeitet.',
+);
+ 
+$messages['de-formal'] = array(
+	'templateedit-showstartingform'       => 'Bitte geben Sie den Artikel ein, den Sie bearbeiten möchten:',
+	'templateedit-showselecttemplateform' => 'Bitte w&auml;hlen Sie die Vorlage, die Sie bearbeiten möchten:',
+	'templateedit-showeditform'           => 'Bitte bearbeiten Sie die Vorlage:'
 );
