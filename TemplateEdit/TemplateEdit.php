cbf11547b12598915e2e5aa69220837eaab38d70
GeneralVolkoV <generalvolkov@gmx.de>
2020-05-04 07:40:23 +0200

    Version Changes for MW 1.34.0

--- TemplateEdit/TemplateEdit.php
+++ TemplateEdit/TemplateEdit.php
@@ -1,43 +1,338 @@
 <?php
-/**
- * TemplateEdit.
- */
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-// credits
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'Template Edit',
-	'version' => '1.1',
-	'author' => array( 'VolkoV' ),
-	'url' => 'http://www.garetien.de',
-	'descriptionmsg'  => 'templateedit-description',
-);
+class TemplateEdit extends SpecialPage {
 
-$rtgIP = dirname( __FILE__ ) . '/';
-$wgHooks['AdminLinks'][] = 'TemplateEdit::addToAdminLinks';
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct( 'TemplateEdit' , 'edit' );
+	}
 
-// This extension uses its own permission type, 'replacetext'
-$wgSpecialPages['TemplateEdit'] = 'TemplateEdit';
-$wgSpecialPageGroups['TemplateEdit'] = 'wiki';
-$wgAutoloadClasses['TemplateEditParser'] = $rtgIP . 'TemplateEditParser.php';
-$wgAutoloadClasses['TemplateEdit']   = $rtgIP . 'SpecialTemplateEdit.php';
+	static public function addToAdminLinks( &$admin_links_tree ) {
+		$general_section = $admin_links_tree->getSection( wfMessage( 'adminlinks_general' )->text() );
+        	$extensions_row = $general_section->getRow( 'extensions' );
+		if ( is_null( $extensions_row ) ) {
+			$extensions_row = new ALRow( 'extensions' );
+			$general_section->addRow( $extensions_row );
+		}
+		$extensions_row->addItem( ALItem::newFromSpecialPage( 'TemplateEdit' ) );
+		return true;
+	}
 
-$wgExtensionAliasesFiles['TemplateEdit'] = $rtgIP . 'TemplateEdit.alias.php';
-$wgExtensionMessagesFiles['TemplateEdit'] = $rtgIP . 'TemplateEdit.i18n.php';
+	static public function onBaseTemplateToolbox( BaseTemplate $skinTemplate, array &$toolbox  ) {
+		$title = $skinTemplate->getSkin()->getRelevantTitle();
+	 	$href = SpecialPage::getTitleFor( 'TemplateEdit' )->getLocalURL(Array("article"=>$title->getPrefixedDBkey()));
+		$toolbox['templateedit'] = array(
+			'msg' => 'templateedit',
+			'href' => $href,
+			'id' => 't-templateedit',
+			'rel' => 'templateedit'
+		);
+	}
+
+	function execute( $subPage ) {
+		global $wgUser, $wgLang;
+		$wgOut=$this->getOutput();
+		$wgRequest=$this->getRequest();
+
+		$this->article=$wgRequest->getText('article');
+		$this->articleprefix=$wgRequest->getText('articleprefix');
+		$this->articleautomatic=$wgRequest->getText('articleautomatic');
+		$this->templatename=$wgRequest->getText('templatename');
+		$this->template=$wgRequest->getText('template');
+		$this->save=$wgRequest->getText('save');
+		$this->user = $wgUser;
+
+		$this->setHeaders();
+
+		//Title valid
+		$title=Title::newFromText($this->article);
+		if($title!=null) {
+			if ( ! $title->userCan( 'edit' ) ) {
+				throw new PermissionsError( 'edit' );
+				return;
+			}
+			$source="";
+			if($title!=null) {
+				$article=new Article($title);
+				if($title->exists()) {
+					//Existing article
+					$wikipage=WikiPage::factory($title);
+					$source=ContentHandler::getContentText($wikipage->getRevision()->getContent());
+				} elseif ($this->templatename!='') {
+					//New article
+					$source="{{".$this->templatename."}}";
+					if($this->articleautomatic=="1")
+						$source.="\n\n(Beschreibungstext fehlt).\n\n{{".$this->templatename." Automatik}}";
+					$content=new WikitextContent( $source );
+					$article->doEditContent( $content,wfMessage('templateedit-templatecreated',$this->templatename)->text());
+				}
+			};
+			//No template selected
+			if($this->template=="") {
+				$this->showSelectTemplateForm($title,$source);
+				return;
+			} else {
+				if($this->save!="1") {
+					//Edit Template
+					$this->showEditForm($title,$source);
+					return;
+				} else {
+					//Save Template
+					$this->showSavedMessage($title,$source,$article);
+					return;
+				}
+			}
+		}
+
+		// if we're still here, show the starting form
+		$this->showStartingForm();
+	}
+
+	function showStartingForm() {
+		$wgOut=$this->getOutput();
+		if ($this->templatename!="")
+			$wgOut->addWikiTextAsContent(wfMessage('templateedit-createwithtemplate',$this->templatename)->text());
+		$wgOut->addHTML(
+			Xml::openElement( 'form', array( 'id' => 'templateedit', 'action' => $this->getPageTitle()->getFullUrl(), 'method' => 'post' ) ) .
+			html::hidden( 'title', $this->getPageTitle()->getPrefixedText() ) .
+			html::hidden( 'templatename', $this->templatename ) .
+			html::hidden( 'articleautomatic', $this->articleautomatic ) .
+			Xml::tags( 'p', null , $this->msg('templateedit-showstartingform')->text() ) .
+			Xml::input( 'article', 50, $this->articleprefix ) .
+			Xml::submitButton(wfMessage('templateedit-continue')->text()) .
+			Xml::closeElement( 'form' )
+		);
+	}
+
+	function showSelectTemplateForm($title,$source) {
+		$wgOut=$this->getOutput();
+		if($source=="") die ('Something is wrong!');
+		$templates=TemplateEditParser::getTemplates($source);
+		$wgOut->addWikiTextAsContent('[[:'.$title->getPrefixedText().']]');
+		$wgOut->addHTML(
+			Xml::openElement( 'form', array( 'id' => 'templateedit', 'action' => $this->getPageTitle()->getFullUrl(), 'method' => 'post' ) ) .
+			html::hidden( 'title',  $this->getPageTitle()->getPrefixedText() ) .
+			html::hidden( 'article', $this->article ).
+			Xml::tags( 'p', null , wfMessage('templateedit-showselecttemplateform')->text() )
+		);
+
+		for($i=0;$i<count($templates);$i++)
+			$wgOut->addHTML( Xml::radio( "template", "$i", $i==0 ,
+				Array('style'=>'margin-left:'.$templates[$i]["level"].'em;') )."<b>".$templates[$i]["name"]."</b> (".$templates[$i]["source"]."...)<br />"
+			);
+
+		$wgOut->addHTML(
+			Xml::submitButton(wfMessage('templateedit-continue')->text()) .
+			Xml::closeElement( 'form' )
+		);
+	}
+
+	function getTemplateDefinitions($template) {
+		$this->deleteold=TRUE;
+		$title=Title::newFromText(wfMessage('templateedit-templateeditortitle',$template)->text());
+		if(($title!=null)&&($title->exists())) {
+			$article=new Article($title);
+			$wikipage=WikiPage::factory($title);
+			$source=ContentHandler::getContentText($wikipage->getRevision()->getContent());
+			$sourceexplode=explode("|",$source);
+			$fields=Array();
+			for($i=0;$i<count($sourceexplode);$i++)
+				if(trim($sourceexplode[$i])!="")
+					$fields[]=trim($sourceexplode[$i]);
+			$indexedFields=Array();
+			for($i=0;$i<count($fields);$i++) {
+				$explode=explode("=",$fields[$i],2);
+				if(count($explode)==1) $explode[]='TEXT!!!';
+				$params=explode("!",$explode[1],6);
+				if(count($params)<1) $params[]='TEXT';
+				if(count($params)<2) $params[]='';
+				if(count($params)<3) $params[]='';
+				if(count($params)<4) $params[]='';
+				if(count($params)<5) $params[]='';
+				if(count($params)<6) $params[]='';
+				$indexedfields[$explode[0]]=Array(
+					'type'        => trim(strip_tags($params[0])),
+					'must'        => (trim(strip_tags($params[1]))=='MUST'),
+					'description' => trim(strip_tags($params[2])),
+					'picklist'    => trim(strip_tags($params[3])),
+					'default'	=> trim(strip_tags($params[4])),
+					'migration'	=> trim(strip_tags($params[5])),
+				);
+			}
+			return $indexedfields;
+		}
+		$this->deleteold=FALSE;
+		return Array(wfMessage('templateedit-noeditor')->text()=>Array('type'=>'TITLE','must'=>FALSE,'description'=>'','picklist'=>'', 'default'=>'', 'migration'=>''));
+	}
+
+	function addInput($wgOut,$name,$def,$value,$style) {
+		$techname="Field".str_replace(' ','',$name);
+		if($def['type']=='TITLE') {
+			$wgOut->addHTML(
+				'<tr><td colspan="2" class="dunkel" style="border-bottom:1px solid gray;"><h3>'.$name.'</h3></td></tr>'
+			);
+			return;
+		}
+		$must='';
+		if($def['must'])
+			$must='<span style="color:red;">'.wfMessage('templateedit-mustfield')->text().'</span> ';
+		$wgOut->addHTML('<tr><td style="border-bottom:1px solid gray;"><b>'.$name.':</b><br/>');
+		$wgOut->addWikiTextAsContent(trim($must.$def['description']));
+		$wgOut->addHTML('</td><td style="border-bottom:1px solid gray;">');
+		if($def['type']=='PICK') {
+			$wgOut->addHTML(
+				Xml::openElement( 'select', array('name'=>$techname,'size'=>'1', 'style' => 'width: 95%;'.$style ) )
+			);
+			$picks=explode(';',$def['picklist']);
+			for($i=0;$i<count($picks);$i++) {
+				$selected=null;
+				if($picks[$i]==trim($value))
+					$selected=Array( 'selected'=>'selected');
+				$wgOut->addHTML(
+					Xml::tags('option', $selected,$picks[$i])
+				);
+			}
+			$wgOut->addHTML(
+				Xml::closeElement( 'select' )
+			);
+		}
+		if($def['type']=='TEXTAREA')
+			$wgOut->addHTML(
+				Xml::textarea( $techname, $value , 50, 5, array( 'style' => 'width: 95%;'.$style ) )
+			);
+		if(($def['type']=='TEXT')||($def['type']=='COORDS')||($def['type']=='LINK')||($def['type']=='LINKNONS')||($def['type']=='NUMBER')) {
+			$newvalue=$value;
+			if($def['type']=='COORDS') {
+				$newvalue=preg_replace('%([0-9\-]*)\s*;\s*([0-9\-]*)\s*;\s*%','$1 $2, ',$value);
+				$newvalue=preg_replace('%;%',' ',$newvalue);
+			}
+			if($def['type']=='LINK') 
+				$newvalue=preg_replace('%\[\[([^:\|\]]*:|)([^\|\]]+)(\|[^\]]*|)\]\]%','$1$2',$value);
+			if($def['type']=='LINKNONS') 
+				$newvalue=preg_replace('%\[\[([^:\|\]]*:|)([^\|\]]+)(\|[^\]]*|)\]\]%','$2',$value);
+			if($def['type']=='NUMBER') 
+				$newvalue=preg_replace('%[^\d]%','',$value);
+			//background-color:#ffff7f;
+			$wgOut->addHTML(
+				Xml::input( $techname, 250, $newvalue , array( 'style' => 'width: 95%;'.$style ) )
+			);
+		}
+		if($def['type']=='OLD')
+			$wgOut->addHTML(
+				Xml::checklabel( wfMessage('templateedit-remove')->text() , 'Remove'.$techname , 'Remove'.$techname , $this->deleteold ) .
+				Xml::input( $techname, 250, $value , array( 'style' => 'width: 95%; background-color:#ff7f7f;'.$style ) )
+			);
+		$wgOut->addHTML('</td></tr>');
+	}
+
+	function showEditForm($title,$source) {
+		$wgOut=$this->getOutput();;
+		if($source=="") die ('Something is wrong!');
+		$templateparts=TemplateEditParser::getTemplateParts($source,$this->template);
+//		print_r($templateparts);
+		$definitions=$this->getTemplateDefinitions($templateparts['template']);
+		$wgOut->addWikiTextAsContent(wfMessage('templateedit-editformintro',$title->getPrefixedText(),$templateparts['template'])->text());
+		$wgOut->addHTML(
+			Xml::openElement( 'form', array( 'id' => 'templateedit', 'action' => $this->getPageTitle()->getFullUrl(), 'method' => 'post' ) ) .
+			Xml::submitButton(wfMessage('templateedit-save')->text()) .
+			html::hidden( 'title',  $this->getPageTitle()->getPrefixedText() ) .
+			html::hidden( 'article', $this->article ).
+			html::hidden( 'template', $this->template ).
+			html::hidden( 'save', "1" ).
+			Xml::tags( 'p', null , wfMessage('templateedit-showeditform')->text() ) .
+			Xml::openElement('table', array('style' => 'border-collapse:collapse;') )
+		);
+		$indexedtp=Array();
+		for($i=0;$i<count($templateparts['params']);$i++) {
+			$indexedtp[$templateparts['params'][$i]['name']]['value']=$templateparts['params'][$i]['value'];
+			$indexedtp[$templateparts['params'][$i]['name']]['used']=FALSE;
+		}
+		foreach($definitions as $name=>$def) {
+			if(isset($indexedtp[$name])) {
+				$value=$indexedtp[$name]['value'];
+				$indexedtp[$name]['used']=TRUE;
+				$background='';
+			} else {
+				$value=$def['default'];
+				$def['description'].=' '.wfMessage('templateedit-fieldundefined')->text();
+				$background='background-color:#ffff7f;';
+			}
+			if($def['migration']!="") {
+				$migs=explode(';',$def['migration']);
+				foreach($migs as $mig)
+					$value.=' '.$indexedtp[$mig]['value'];
+				$value=trim($value);
+			}
+			$this->addInput($wgOut,$name,$def,$value,$background);
+		};
+		$def=Array('type'=>'TITLE','must'=>FALSE,'description'=>'','picklist'=>'');
+		$this->addInput($wgOut,wfMessage('templateedit-oldfieldstitle')->text(),$def,'','');
+		foreach($indexedtp as $name=>$field)
+			if(!$field['used']) {
+			$def=Array('type'=>'OLD','must'=>FALSE,'description'=>wfMessage('templateedit-oldfield')->text(),'picklist'=>'');
+			$this->addInput($wgOut,$name,$def,$field['value'],'');
+		};
+
+		$wgOut->addHTML(
+			Xml::closeElement( 'table' ) .
+			Xml::submitButton(wfMessage('templateedit-save')->text()) .
+			Xml::closeElement( 'form' )
+		);
+	}
+
+	function showSavedMessage($title,$source,$article) {
+		$wgRequest=$this->getRequest();;
+		$wgOut=$this->getOutput();
+		if($source=="") die ('Something is wrong!');
+		$templateparts=TemplateEditParser::getTemplateParts($source,$this->template);
+		$definitions=$this->getTemplateDefinitions($templateparts['template']);
+		$indexedtp=Array();
+		for($i=0;$i<count($templateparts['params']);$i++) {
+			$indexedtp[$templateparts['params'][$i]['name']]['value']=$templateparts['params'][$i]['value'];
+			$indexedtp[$templateparts['params'][$i]['name']]['used']=FALSE;
+		}
+		foreach($definitions as $name=>$def) {
+			$techname="Field".str_replace(' ','',$name);
+			$definitions[$name]['value']=$wgRequest->getText($techname);
+			if(isset($indexedtp[$name]))
+				$indexedtp[$name]['used']=TRUE;
+		}
+		foreach($indexedtp as $name=>$field) {
+			$techname="Field".str_replace(' ','',$name);
+			if((!$field['used'])&&($wgRequest->getText('Remove'.$techname)!='1')) {
+				$definitions[$name]=Array(
+					'type'=>'OLD','must'=>FALSE,'description'=>wfMessage('templateedit-oldfield')->text(),'picklist'=>'',
+					'value'=>$wgRequest->getText($techname)
+				);
+			}
+		}
+		$insert=$templateparts['template'];
+		$i=1;
+		foreach($definitions as $name=>$def) {
+			if($def['type']!='TITLE') {
+				if($name==$i)
+					$insert.='|'.$def['value'];
+				else {
+					if($i==1)
+						$insert.="\n";
+					$insert.='|'.$name.'='.$def['value']."\n";
+				}
+				$i++;
+			}
+		}
+
+		$newsource=TemplateEditParser::replaceTemplate($source,$this->template,$insert);
+		$content=new WikitextContent( $newsource );
+		$article->doEditContent( $content,wfMessage('templateedit-templateedited',$templateparts['template'])->text());
+		$wgOut->addWikiTextAsContent('[[:'.$title->getPrefixedText().']]');
+		$wgOut->addHTML(
+			Xml::tags( 'p', null , wfMessage('templateedit-articlesaved')->text() )
+		);
+	}
 
 
-// Add a shortcut link to the toolbox.
-$wgHooks['BaseTemplateToolbox'][] = function( BaseTemplate $skinTemplate, array &$toolbox  ) {
-	$title = $skinTemplate->getSkin()->getRelevantTitle();
- 	$href = SpecialPage::getTitleFor( 'TemplateEdit' )->getLocalURL(Array("article"=>$title->getPrefixedDBkey()));
-    $toolbox['templateedit'] = array(
-      'msg' => 'templateedit',
-      'href' => $href,
-      'id' => 't-templateedit',
-      'rel' => 'templateedit'
-    );
-};
-
-
+}
